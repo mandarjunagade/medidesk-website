52840da5adf2016f0284b0068f986fc092ed0279
Maddyy <mandar.junagade@gmail.com>
2025-11-27 18:04:09 +0100

    Initial Commit

---
+++ lohrbach/index.php
@@ -0,0 +1,11 @@
+<?php include 'includes/meta.php'; ?>
+<title>Lohrbach</title>
+</head>
+
+<body>
+
+<script id="medidesk-widget-script" src="https://widget.medideskr.cloud/widget.iife.js" data-clinic-id="691b5e9d7703e8c7c88fa8d4"></script>
+
+</body>
+
+</html>
