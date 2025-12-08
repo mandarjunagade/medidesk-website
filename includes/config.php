2ce91330e8769b36388845655c8a5e1c3e9ed80f
Maddyy <mandar.junagade@gmail.com>
2025-12-08 13:14:28 +0100

    Initial Commit

--- includes/config.php
+++ includes/config.php
@@ -3,6 +3,6 @@ $root="https://medideskr.cloud/";
 $mail="info@medideskr.cloud";
 $mail2="";
 $phoneno="0172-4056551";
-$brandname="MediDesk";
+$brandname="MediDesk®";
 $address="Hambergsfeld 8, D-21394 Westergellersen";
 ?>
