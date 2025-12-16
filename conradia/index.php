4e6605607b176e09134df17b2f6a5c231228de40
Maddyy <mandar.junagade@gmail.com>
2025-12-16 11:47:02 +0100

    Conradia push

---
+++ conradia/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Lohrbach</title>
+</head>
+
+<body>
+
+<script id="medidesk-widget-script" src="https://widget.medideskr.cloud/widget.iife.js" data-clinic-id="69356544eb6b551247a86fab"></script>
+
+</body>
+
+</html>
