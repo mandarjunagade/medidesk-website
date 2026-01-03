df09512168e0a7aa1a3caa2e5f46c7e7ffcc4052
Maddyy <mandar.junagade@gmail.com>
2026-01-03 11:12:46 +0100

    autohaus and soltau

---
+++ autohaus/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Autohaus Wissmann</title>
+</head>
+
+<body>
+
+<script id="medidesk-widget-script" src="https://widget.medideskr.cloud/widget.iife.js" data-clinic-id="6957b563cccfe5db168d3881"></script>
+
+</body>
+
+</html>
---
+++ soltau/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Soltau</title>
+</head>
+
+<body>
+
+<script id="medidesk-widget-script" src="https://widget.medideskr.cloud/widget.iife.js" data-clinic-id="6957c139cccfe5db168d4897"></script>
+
+</body>
+
+</html>
