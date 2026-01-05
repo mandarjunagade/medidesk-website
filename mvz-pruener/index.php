96a5fb63b32151035c25eb7fd96e591c08bda1e0
Maddyy <mandar.junagade@gmail.com>
2026-01-05 10:15:43 +0100

    MVZ pruner web

---
+++ mvz-pruener/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>MVZ Prüner Gang</title>
+</head>
+
+<body>
+
+<script id="medidesk-widget-script" src="https://widget.medideskr.cloud/widget.iife.js" data-clinic-id="69525fdacccfe5db168cf269"></script>
+
+</body>
+
+</html>
