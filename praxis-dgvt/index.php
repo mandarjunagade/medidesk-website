2bdb1bcf46e3a7d979d72411f6800229c6fc0328
Maddyy <mandar.junagade@gmail.com>
2026-02-17 15:25:28 +0100

    dgvt

---
+++ praxis-dgvt/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Praxis DGVT</title>
+</head>
+
+<body>
+
+<script id="medidesk-widget-script" src="https://widget.medideskr.cloud/widget.iife.js" data-clinic-id="69946325cccfe5db168fa4fc"></script>
+
+</body>
+
+</html>
