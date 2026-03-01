86ef212365feb35b8fdbaf665454dcff6199b692
mandarjunagade <83273430+mandarjunagade@users.noreply.github.com>
2026-03-01 19:31:07 +0100

    Add configuration file for Praxis DGVT

---
+++ praxis-dgvt/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'openai_api_key'    => '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',
+    'vector_store_id'   => 'vs_69a46a2bf81881919cc5744264c20c3f',
+    'model'             => 'gpt-4.1',
+    'max_tokens'        => 2048,
+    'temperature'       => 1,
+    'top_p'             => 1,
+    'practice_name'     => 'Praxis DGVT Erlangen',
+    'medidesk_clinic_id'=> '69946325cccfe5db168fa4fc',
+];
